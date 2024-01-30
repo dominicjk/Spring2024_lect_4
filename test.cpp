034872a19e688de66714c29bf8dea6459197790a
dominicjk <102314055+dominicjk@users.noreply.github.com>
2024-01-30 21:47:56 +0000

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,17 +9,17 @@ unsigned int Factorial( unsigned int number ) {
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
 
-    vector<int> vect{0, 0, 0};
+    vector<double> vect{0, 0, 0};
 
     REQUIRE( AddN(vect, 3).size() == 3 );
     REQUIRE( AddN(vect, 3)[0] == 3 );
     REQUIRE(AddN(vect, 4)[1] == 4);
 
-    vector<int> vect2{1, 2, 3, 4};
+    vector<double> vect2{1, 2, 3, 4};
     REQUIRE(SubtractN(vect2, 5).size() == 4);
     REQUIRE(SubtractN(vect2, 4)[1] == -2);
 
-    vector<int> correctVec{-3, -2, -1, 0};
+    vector<double> correctVec{-3, -2, -1, 0};
     for (int i = 0; i < correctVec.size(); i++) {
         REQUIRE(SubtractN(vect2, 4)[i] == correctVec[i]);
     }
